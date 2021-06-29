2c812fad599189eaad94fd966bbee7198be87269
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-06-29 14:05:15 +0530

    crud completed

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty } from '@nestjs/swagger';
+import { IsAlphanumeric, MaxLength } from 'class-validator';
 
 /* eslint-disable prettier/prettier */
 export class CreateUserDto {
   @ApiProperty()
+  @IsAlphanumeric()
+  @MaxLength(10)
   name: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Get, NotFoundException, Param } from '@nestjs/common';
 import { Query } from '@nestjs/common';
 import { Post } from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import {
+  ApiBadRequestResponse,
   ApiCreatedResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
@@ -16,23 +18,28 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
   @ApiOkResponse({ type: User, isArray: true })
   @ApiQuery({ name: 'name', required: false })
   @Get()
   getUsers(@Query('name') name: string): User[] {
     return this.usersService.findAll(name);
   }
+
   @ApiOkResponse({ type: User })
   @ApiNotFoundResponse()
   @Get(':id')
-  getUsersById(@Param('id') id: string): User {
+  getUsersById(@Param('id', ParseIntPipe) id: number): User {
+    console.log(typeof id);
     const user = this.usersService.findById(Number(id));
     if (!user) {
       throw new NotFoundException();
     }
     return user;
   }
+
   @ApiCreatedResponse({ type: User })
+  @ApiBadRequestResponse()
   @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
